5852a1b46d31322c8db0e314e2c2b8650479f601
Rana Muhammad Tayyab Atiq <135827062+Tayyab2344@users.noreply.github.com>
2025-08-28 19:53:07 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({ origin: "https://mern-memory-game-9v6a-git-main-tayyabs-projects-9d235f55.vercel.app", credentials: true }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/games", gameRouter);
